9e7e526406df14d515ce08c6cf537440355038ec
Anderson Fontes Fernandes Júnior <andersonfontes795@gmail.com>
2025-05-22 19:27:27 -0300

    Delete http 5 directory

--- http 5/server/src/routes/mega.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { last } from "../controllers/MegaController";
-import { cors } from "../middlewares/cors";
-
-const router = Router();
-
-router.get("/megasena", cors, last);
-
-export default router;
