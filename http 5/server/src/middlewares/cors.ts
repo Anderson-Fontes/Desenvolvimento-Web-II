689b496e69466bf391d1aa494e15b153a1d724a8
Anderson Fontes Fernandes Júnior <andersonfontes795@gmail.com>
2025-06-03 21:16:23 -0300

    Delete http 5 directory

--- http 5/server/src/middlewares/cors.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const cors = (req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Ou especifique seu frontend
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-};
